e26e9d1072d6941264fa491ae878ff3b69c786e0
김승우 <tmddnnm0315@naver.com>
2023-04-25 20:47:30 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@
 
 const express = require("express");
 const app = express();
-const home = require('./routes/home')//라우팅
+const home = require('./src/routes/home')//라우팅
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use('/', home);//미들웨어 등록하는 메소드
